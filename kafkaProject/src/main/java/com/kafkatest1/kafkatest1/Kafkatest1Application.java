255f15447a1dca639c17bbf1d26663e6aee3e4df
GebreReda <109552531+GebreReda@users.noreply.github.com>
2023-06-20 21:01:44 -0500

    Add files via upload

---
+++ kafkaProject/src/main/java/com/kafkatest1/kafkatest1/Kafkatest1Application.java
@@ -0,0 +1,13 @@
+package com.kafkatest1.kafkatest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafkatest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafkatest1Application.class, args);
+	}
+
+}
---
+++ kafkaProject/src/test/java/com/kafkatest1/kafkatest1/Kafkatest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkatest1.kafkatest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafkatest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
